738b03bbb55f6611b556751c1c7270fd4565472a
Ali <alikreku@protonmail.com>
2023-12-22 13:56:31 +0100

    created crud functionalities

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,29 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy", policy =>
-    {
-        // policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5000");
-        // policy.WithOrigins("http://localhost:5000")
-        // .AllowAnyHeader()
-        // .AllowAnyMethod();
-        // policy.WithOrigins("http://127.0.0.1:3000")
-        // .AllowAnyHeader()
-        // .AllowAnyMethod();
-        policy.WithOrigins("http://localhost:3000")
-        .AllowAnyHeader()
-        .AllowAnyMethod();
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
